72640b1a77901cceceda29b657c04e4c260c5c54
Ivan Belousov <105873927+ib729@users.noreply.github.com>
2025-10-03 01:40:57 +0800

    Add cask file

---
+++ Casks/yt-dlp-gui.rb
@@ -0,0 +1,13 @@
+cask "yt-dlp-gui" do
+  version "1.1.0"
+  sha256 "6e27283a15737b629544dcbde50315b7c6e9bfbf0b87ddda829c2ff78a87a872"
+
+  url "https://github.com/ib729/yt-dlp-gui/releases/download/v#{version}/yt-dlp-gui-#{version}.dmg"
+  
+  name "yt-dlp-gui"
+  desc "A native SwiftUI interface for yt-dlp."
+  homepage "https://github.com/ib729/yt-dlp-gui"
+
+  app "yt-dlp-gui.app"
+end
+
